aa1db83d6714ce586045a3d0eae7f5f169ae5542
Fikri Salam <intiuxcom@gmail.com>
2018-12-22 16:52:07 +0700

    Initial Commit

---
+++ HumanApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using HumanApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HumanApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
